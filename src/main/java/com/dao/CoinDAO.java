5104cd2798a5ab44f76b7f7e1c6a591b6cc7e7b9
shin <shs2797@naver.com>
2018-04-16 15:59:36 +0900

    dddd

---
+++ src/main/java/com/dao/CoinDAO.java
@@ -0,0 +1,23 @@
+package com.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CoinDAO {
+
+	@Autowired
+	SqlSessionTemplate template;
+	
+	public void bitinsert(String bit) {
+		template.insert("CoinMapper.bitInsert",bit);
+	}
+	
+	public int bitCount() {
+		return template.selectOne("CoinMapper.bitCount");
+	}
+	
+	
+	
+}
